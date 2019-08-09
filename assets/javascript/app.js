e757b0a6fbec143efae3873d777180eea2e94ab7
SpeedHunterSam <brian.s2@gmail.com>
2019-08-08 21:57:14 -0500

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,39 @@
 
-function startGame(){
 
-    document.getElementById("gameContainer").innerHTML = 'GAME STARTED. Good Luck!<br/> 
-    <div class="col-lg-12" id="questions">
-            
-    <p>Which of these superheroes was created by Marvel?</p>';
+let timer = 2; // set timer to 59 seconds
+let timerRunning = true;
 
+
+function startGame() {
+
+    document.getElementById("gameContainer").innerHTML = timer + " Seconds left"; //display starting timer
+
+    if(timerRunning === true){
+        setInterval(updateCountdown, 1000);
+        console.log(timer);
+        console.log(timerRunning);
+        }else{
+            return;
+        }
+}
+
+function gameOver() {
+
+    document.getElementById("gameContainer").innerHTML = "Game Over";
 
 }
 
+function updateCountdown() {
+
+    console.log(timer);
+    console.log(timerRunning);
+    document.getElementById("gameContainer").innerHTML = timer + " Seconds left";
+    if (timer <= 0) {
+        timerRunning = false;
+        gameOver();
+    }else{
+        timer--;
+    }
+}
 
 document.getElementById("goToGame").addEventListener("click", startGame); //listen for initialization click to start the game
