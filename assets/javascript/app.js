9e88747c1c968432456c995a1c8a5d0be0b2b01a
SpeedHunterSam <brian.s2@gmail.com>
2019-08-08 22:31:21 -0500

    cleaned up code and made sure all processes stop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,32 +8,35 @@ function startGame() {
 
     document.getElementById("gameContainer").innerHTML = timer + " Seconds left"; //display starting timer
 
-    if(timerRunning === true){
-        setInterval(updateCountdown, 1000);
-        console.log(timer);
+        const fullStop = setInterval(updateCountdown, 1000);  //set interval to run countdown function
+        console.log("Game Function Running"); //a check to get status of timer in console
         console.log(timerRunning);
-        }else{
-            return;
-        }
-}
 
-function gameOver() {
+    function gameOver() {
 
-    document.getElementById("gameContainer").innerHTML = "Game Over";
+        //game over function will inform user that game is over, stop the timer and display the score
+        console.log(timerRunning) //a double check to make sure everything stops
+        console.log("Game has Stopped")
+        clearInterval(fullStop);
+        document.getElementById("gameContainer").innerHTML = "Game Over";
 
-}
 
-function updateCountdown() {
-
-    console.log(timer);
-    console.log(timerRunning);
-    document.getElementById("gameContainer").innerHTML = timer + " Seconds left";
-    if (timer <= 0) {
-        timerRunning = false;
-        gameOver();
-    }else{
-        timer--;
     }
+
+    function updateCountdown() {
+
+        //this function displays time remaining to the viewport
+
+        document.getElementById("gameContainer").innerHTML = timer + " Seconds left";
+        if (timer <= 0) {
+            timerRunning = false;
+            console.log(timerRunning);  //a check to make sure everything stops
+            gameOver();
+        } else {
+            timer--;
+        }
+    }
+
 }
 
 document.getElementById("goToGame").addEventListener("click", startGame); //listen for initialization click to start the game
